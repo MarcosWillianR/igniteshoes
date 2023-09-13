650e13f1299650919fee5ab370ebdbf2808c9806
Marcos Willian <markusuuuu@gmail.com>
2023-09-12 21:52:50 -0300

    refactor: put Notification component and logic on route index, to handle navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useState } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
-import { Notification } from './src/components/Notification';
 import { tagUserInfoCreate } from './src/notifications/notificationTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
@@ -19,35 +17,16 @@ OneSignal.setAppId(oneSignalAppId);
 OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
-  const [notification, setNotification] = useState<OSNotification>();
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate();
 
-  useEffect(() => {
-    const unsubscribe = OneSignal
-    .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent ) => {
-      const response = notificationReceivedEvent.getNotification();
-      setNotification(response);
-    })
-
-    return () => unsubscribe;
-  }, [])
-
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-      barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
-
-      {notification?.title && (
-        <Notification title={notification.title} onClose={() => setNotification(undefined)} />
-      )}
     </NativeBaseProvider>
   );
 }
