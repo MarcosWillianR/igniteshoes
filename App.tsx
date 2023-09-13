364c8cc9af8423a2cf5eafc920045964917c1e8b
Marcos Willian <markusuuuu@gmail.com>
2023-09-12 21:39:16 -0300

    chore: showing foreground notifications using OneSignal + Notification component.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Platform, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import OneSignal from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent, OSNotification } from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { Notification } from './src/components/Notification';
 import { tagUserInfoCreate } from './src/notifications/notificationTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
@@ -15,15 +16,22 @@ import { CartContextProvider } from './src/contexts/CartContext';
 const oneSignalAppId = Platform.OS === 'ios' ? "IOS_APP_ID" : "ANDROID_APP_ID";
 OneSignal.setAppId(oneSignalAppId);
 
-OneSignal.promptForPushNotificationsWithUserResponse(response => {
-  console.log(response);
-})
+OneSignal.promptForPushNotificationsWithUserResponse();
 
 export default function App() {
+  const [notification, setNotification] = useState<OSNotification>();
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
+  tagUserInfoCreate();
+
   useEffect(() => {
-    tagUserInfoCreate();
+    const unsubscribe = OneSignal
+    .setNotificationWillShowInForegroundHandler((notificationReceivedEvent: NotificationReceivedEvent ) => {
+      const response = notificationReceivedEvent.getNotification();
+      setNotification(response);
+    })
+
+    return () => unsubscribe;
   }, [])
 
   return (
@@ -36,6 +44,10 @@ export default function App() {
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
+
+      {notification?.title && (
+        <Notification title={notification.title} onClose={() => setNotification(undefined)} />
+      )}
     </NativeBaseProvider>
   );
 }
